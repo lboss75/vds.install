d4e0d897bc87ad05a4449d72a890fcb082d86349
Vadim Malyshev <lboss75@gmail.com>
2020-05-05 11:37:33 +0300

    update

--- src/win_install/build.bat
+++ src/win_install/build.bat
@@ -1,6 +1,6 @@
 set PATH=%PATH%;..\..\..\wix\
-candle.exe -ext WixUIExtension -ext WixUtilExtension -dLanguage=1033 -dSourceDir=..\..\..\vds\build -dSourceDirTools=..\..\..\vds.net -dConfig=Debug -dCulture=en-us vds.wxs -out out\vds-en.wixobj
+candle.exe -ext WixUIExtension -ext WixUtilExtension -dLanguage=1033 -dSourceDir=..\..\..\vds\build -dSourceDirTools=..\..\..\vds.net -dConfig=Release -dCulture=en-us vds.wxs -out out\vds-en.wixobj
 light.exe -ext WixUIExtension -ext WixUtilExtension out\vds-en.wixobj -cultures:en-us -loc vds.en-us.wxl -out out\vds-en-us.msi
 
-candle.exe -ext WixUIExtension -ext WixUtilExtension -dLanguage=1049 -dSourceDir=..\..\..\vds\build -dSourceDirTools=..\..\..\vds.net -dConfig=Debug -dCulture=ru-ru vds.wxs -out out\vds-ru.wixobj
+candle.exe -ext WixUIExtension -ext WixUtilExtension -dLanguage=1049 -dSourceDir=..\..\..\vds\build -dSourceDirTools=..\..\..\vds.net -dConfig=Release -dCulture=ru-ru vds.wxs -out out\vds-ru.wixobj
 light.exe -ext WixUIExtension -ext WixUtilExtension out\vds-ru.wixobj -cultures:ru-ru -loc vds.ru-ru.wxl -out out\vds-ru-ru.msi
---
+++ src/win_install/build_sources.bat
@@ -0,0 +1,17 @@
+echo on
+set root_folder=%~d0%~p0
+
+set SourceDir=%root_folder%..\..\..\vds
+call %SourceDir%\setenv.bat
+cd %SourceDir%\build
+devenv vds.sln /Build Release /Project vds_windows 
+
+set SourceDirTools=%root_folder%..\..\..\vds.net
+cd %SourceDirTools%\src\client\IVySoft.VDS.Client.UI.WPF
+dotnet build IVySoft.VDS.Client.UI.WPF.csproj -c Release 
+
+cd %SourceDirTools%\src\client\IVySoft.VDS.Client.UI.WPF.Wallet
+dotnet build IVySoft.VDS.Client.UI.WPF.Wallet.csproj -c Release 
+
+cd %SourceDirTools%\src\client\IVySoft.VDS.Client.UI.WPF.Disk
+dotnet build IVySoft.VDS.Client.UI.WPF.Disk.csproj -c Release 
